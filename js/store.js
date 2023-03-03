cad2b605f65b0c180c2a862e4fb6d488a1aa443e
Michael Coyle <michael.david.coyle4@gmail.com>
2023-03-02 23:43:44 -0600

    make html template from first item on store.html, add data attributes and create DOM reference in store.js

--- js/store.js
+++ js/store.js
@@ -1,5 +1,8 @@
 import items from '../items.json';
 
+const storeItemTemplate = document.querySelector('#store-item-template');
+
 export function setupStore() {
     console.log(items);
+    console.log(storeItemTemplate);
 }
