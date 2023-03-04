aa6ef8cf10c9af5c5283fe0258ade27ec3cae8cc
Michael Coyle <michael.david.coyle4@gmail.com>
2023-03-03 23:14:10 -0600

    remove hard coded store items and create funtions to populate store from items.json

--- js/store.js
+++ js/store.js
@@ -1,8 +1,31 @@
 import items from '../items.json';
+import formatCurrency from "./formatCurrency";
 
 const storeItemTemplate = document.querySelector('#store-item-template');
+const storeItemContainer = document.querySelector('[data-store-container]');
+const IMAGE_URL = 'https://dummyimage.com/420x260';
 
 export function setupStore() {
-    console.log(items);
-    console.log(storeItemTemplate);
+    items.forEach(renderStoreItem);
+}
+
+function renderStoreItem(item) {
+    const storeItem = storeItemTemplate.content.cloneNode(true);
+
+    const itemContainer = storeItem.querySelector('[data-store-item]');
+    itemContainer.dataset.itemId = item.id;
+
+    const name = storeItem.querySelector('[data-name]');
+    name.innerText = item.name;
+
+    const category = storeItem.querySelector('[data-category]');
+    category.innerText = item.category;
+
+    const image = storeItem.querySelector('[data-image]');
+    image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`;
+
+    const price = storeItem.querySelector('[data-price]');
+    price.innerText = formatCurrency(item.priceCents / 100);
+
+    storeItemContainer.appendChild(storeItem);
 }
