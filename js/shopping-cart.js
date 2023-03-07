be7456f06a38771b6f73974c0f808e671cfe02f8
Michael Coyle <michael.david.coyle4@gmail.com>
2023-03-06 22:44:47 -0600

    add event listener to hide/show cart when cart button is clicked

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,10 +1,16 @@
+const cartButton = document.querySelector('[data-cart-button]');
+const cartItemsContainer = document.querySelector('[data-cart-items-container]');
+
 export function setupShoppingCart() {}
 
 // Add items to cart
 // Remove items from cart
-// Show/hide cart when clicked
 // Show/hide cart button when it has no items or on
 //     when it goes from 0 to 1 item
 // Persist across multiple pages
 // Calculate an accurate total
 // Handle multiples of the same item added to cart
+
+cartButton.addEventListener('click', () => {
+    cartItemsContainer.classList.toggle('invisible');
+});
